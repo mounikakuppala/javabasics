c5c899a7752f4848958df3c4ff1045a7295252c7
mounikakuppala549 <mounikakuppala549@gmail.com>
2026-07-28 10:56:27 +0530

    swapping two numbers

---
+++ corejava/src/com/Constructors/SwappingTwoNumbers.java
@@ -0,0 +1,18 @@
+package com.Constructors;
+
+public class SwappingTwoNumbers {
+
+	public static void main(String[] args) {
+		
+       int a=10;
+       int b=20;
+       System.out.println("Before swapping A value is:"+a);
+       System.out.println("Berore swapping B value is :"+b);
+       a=a+b;//30
+       b=a-b;//20
+       a=a-b;//10
+       System.out.println(" After swapping A value is:"+a);
+       System.out.println("After swapping B value is:"+b);
+       }
+
+}
